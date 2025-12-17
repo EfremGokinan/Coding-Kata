609410f3c5e68a165f07f230c42cb87f8e8074e4
Efrem Gokinan <efrem_gokinan@hotmail.com>
2025-12-17 15:04:55 +0100

    changed logic of legendary item

--- TypeScript/src/gilded-tros.ts
+++ TypeScript/src/gilded-tros.ts
@@ -15,11 +15,15 @@ export class GildedTros {
                 this.items[i].name == 'Long Methods' ||
                 this.items[i].name == 'Ugly Variable Names';
 
+            // NEW: Legendary must always have Quality 80
+            if (this.items[i].name == 'B-DAWG Keychain') {
+                this.items[i].quality = 80;
+                continue;
+            }
+
             if (this.items[i].name != 'Good Wine' && this.items[i].name != 'Backstage passes for Re:Factor' && this.items[i].name != 'Backstage passes for HAXX') {
                 if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'B-DAWG Keychain') {
                         this.items[i].quality = this.items[i].quality - 1;
-                    }
                 }
             } else {
                 if (this.items[i].quality < 50) {
